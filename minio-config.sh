f39aab094a201c8378a13d5767b12e7444d53c1b
Dejan <55488175+dejandro@users.noreply.github.com>
2023-11-06 15:55:12 +0100

    Update minio-config.sh

--- minio-config.sh
+++ minio-config.sh
@@ -9,32 +9,22 @@ chmod +x $HOME/minio-binaries/mc
 echo "export PATH=\$PATH:\$HOME/minio-binaries/" >> .bashrc
 cd $HOME/minio-binaries/
 
-#create the Alias for the FIRST and SECOND minio instances
+#create the Alias for the minio instances
 echo ""
 while true; do
-     read -p "Do you want to create an Alias for the FIRST minio instance? [Y/n]" firstinput
-     case $firstinput in
-        [yY]* )  read -p "Enter your Access key for FIRST `echo $'\n> '`" accessfirst;
-                 read -p "Enter your Secret key for FIRST `echo $'\n> '`" secretfirst;
-                 ./mc alias set FIRST http://IP-ADDRESS-OF-THE-FIRST-INSTANCE:PORT $accessfirst $secretfirst; break;;
+     read -p "Do you want to create a new alias for the minio instance? [Y/n]" answer
+     case $answer in
+        [yY]* )  read -p "Enter the preferred name for the instance `echo $'\n> '`" instancename;
+                 read -p "Enter your Access key for $instancename `echo $'\n> '`" accesskey;
+                 read -p "Enter your Secret key for $instancename `echo $'\n> '`" secretkey;
+                 read -p "Enter the http://IP-ADDRESS-OF-THE-INSTANCE:PORT for $instancename (e.g. http://127.0.0.1:9000) `echo $'\n> '`" ipaddr;
+                 ./mc alias set $instancename $ipaddr $accesskey $secretkey;;
         [nN]* )  echo -e "\nSkipped"; break;;
         * ) echo -e "\nInvalid response";;
      esac
 done
-echo ""
-while true; do
-     read -p "Do you want to create an Alias for the SECOND minio instance? [Y/n]" secondinput
-     case $secondinput in
-        [yY]* )  read -p "Enter your Access key for SECOND `echo $'\n> '`" accesssecond;
-                 read -p "Enter your Secret key for SECOND `echo $'\n> '`" secretsecond;
-                 ./mc alias set SECOND http://IP-ADDRESS-OF-THE-SECOND-INSTANCE:PORT $accesssecond $secretsecond; break;;
-        [nN]* )  echo -e "\nSkipped"; break;;
-        * ) echo -e "\nInvalid response";;
-     esac
-done
-
-echo -e "\nTest and list buckets in FIRST\n"
-./mc ls FIRST
+echo -e "\nTest and list buckets in $instancename\n"
+./mc ls $instancename
 
 cd $HOME/minio-binaries/
 bash

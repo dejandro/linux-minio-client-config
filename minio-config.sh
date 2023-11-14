a53f6e87f093dda62f932479aedcfc4b8116d9e1
Dejan <55488175+dejandro@users.noreply.github.com>
2023-11-14 14:02:32 +0100

    Update minio-config.sh

--- minio-config.sh
+++ minio-config.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 #install the mc command line tool
-curl https://dl.min.io/client/mc/release/linux-amd64/mc \
-  --create-dirs \
-  -o $HOME/minio-binaries/mc
+if [[ ! -d "$HOME/minio-binaries/" ]] ; then
+    curl https://dl.min.io/client/mc/release/linux-amd64/mc \
+    --create-dirs \
+    -o $HOME/minio-binaries/mc
 
-chmod +x $HOME/minio-binaries/mc
-echo "export PATH=\$PATH:\$HOME/minio-binaries/" >> .bashrc
-cd $HOME/minio-binaries/
+    chmod +x $HOME/minio-binaries/mc
+    echo "export PATH=\$PATH:\$HOME/minio-binaries/" >> .bashrc
+    cd $HOME/minio-binaries/
+else
+    cd $HOME/minio-binaries/
 
 #create the Alias for the minio instances
 echo ""
 while true; do
-     read -p "Do you want to create a new alias for the minio instance? [Y/n]" answer
+     read -p "Do you want to create new alias for the minio instance? [Y/n]" answer
      case $answer in
         [yY]* )  read -p "Enter the preferred name for the instance `echo $'\n> '`" instancename;
                  read -p "Enter your Access key for $instancename `echo $'\n> '`" accesskey;
                  read -p "Enter your Secret key for $instancename `echo $'\n> '`" secretkey;
-                 read -p "Enter the http://IP-ADDRESS-OF-THE-INSTANCE:PORT for $instancename (e.g. http://127.0.0.1:9000) `echo $'\n> '`" ipaddr;
+                 read -p "Enter the http://IP-ADDRESS-OF-THE-INSTANCE:PORT for $instancename (e.g. http://12>
                  ./mc alias set $instancename $ipaddr $accesskey $secretkey;;
         [nN]* )  echo -e "\nSkipped"; break;;
         * ) echo -e "\nInvalid response";;
@@ -28,3 +31,5 @@ echo -e "\nTest and list buckets in $instancename\n"
 
 cd $HOME/minio-binaries/
 bash
+
+fi

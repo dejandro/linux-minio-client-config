43013e64c40a5e9b33664aeef0415e9bbadb9a93
Dejan <55488175+dejandro@users.noreply.github.com>
2023-11-20 12:23:39 +0100

    Update minio-config.sh

--- minio-config.sh
+++ minio-config.sh
@@ -4,12 +4,12 @@
 if [[ ! -d "$HOME/minio-binaries/" ]] ; then
     curl https://dl.min.io/client/mc/release/linux-amd64/mc \
     --create-dirs \
-    -o $HOME/minio-binaries/mc
+   -o $HOME/minio-binaries/mc
 
     chmod +x $HOME/minio-binaries/mc
     echo "export PATH=\$PATH:\$HOME/minio-binaries/" >> .bashrc
     cd $HOME/minio-binaries/
-else
+  else
     cd $HOME/minio-binaries/
 
 #create the Alias for the minio instances
@@ -20,7 +20,7 @@ while true; do
         [yY]* )  read -p "Enter the preferred name for the instance `echo $'\n> '`" instancename;
                  read -p "Enter your Access key for $instancename `echo $'\n> '`" accesskey;
                  read -p "Enter your Secret key for $instancename `echo $'\n> '`" secretkey;
-                 read -p "Enter the http://IP-ADDRESS-OF-THE-INSTANCE:PORT for $instancename (e.g. http://12>
+                 read -p "Enter the http://IP-ADDRESS-OF-THE-INSTANCE:PORT for $instancename (e.g. http://127.0.0.1:9000) `echo $'\n> '`" ipaddr;
                  ./mc alias set $instancename $ipaddr $accesskey $secretkey;;
         [nN]* )  echo -e "\nSkipped"; break;;
         * ) echo -e "\nInvalid response";;
